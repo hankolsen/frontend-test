8d2ead57ee365b5f81b68bae103b8f9efa68285c
hank <hank.delang@enfogroup.com>
2019-01-02 17:38:31 +0100

    Add initial state for task 2

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const handler = (event) => {
+  // YOUR CODE HERE
+  // Hint: It is ok to just console log the updated password value,
+  // but you get a gold star if you actually submit the form with the updated value.
+};
+
